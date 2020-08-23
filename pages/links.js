0bb0af4a9c94d8943356f9d6f81b66f87891cac8
Chris Anderson <chris@fastmail.in>
2020-08-23 07:14:49 -0400

    Transition on links

--- pages/links.js
+++ pages/links.js
@@ -1,13 +1,19 @@
+import Link from 'next/link'
+
 export default function Links() {
   const name = 'Chris Anderson'
   return (
     <div className="max-w-xs px-4 mt-12 mx-auto mb-24">
       <div className="flex flex-col items-center">
-        <img
-          src="/images/logo.png"
-          className="h-32 w-32 rounded-full"
-          alt={name}
-        />
+        <Link href="/">
+          <a>
+            <img
+              src="/images/logo.png"
+              className="h-32 w-32 rounded-full"
+              alt={name}
+            />
+          </a>
+        </Link>
         <h1 className="text-6xl leading-10 font-black tracking-tighter my-4">
           Links to
           <br />
@@ -15,15 +21,33 @@ export default function Links() {
         </h1>
       </div>
       <div className="flex flex-col my-4 mx-6">
-        <a className="text-3xl font-bold">Projects</a>
-        <a className="text-2xl">GARD</a>
-        <a className="text-2xl">MyTotem</a>
-        <a className="text-2xl">OneTrust</a>
-        <a className="text-2xl">Seize The Market</a>
-        <a className="text-2xl">HylasFit</a>
-        <a className="text-3xl font-bold">Resume</a>
-        <a className="text-3xl font-bold">Blog</a>
-        <a className="text-3xl font-bold">Instagram</a>
+        <a className="text-3xl font-bold transition ease-in-out duration-200 hover:text-blue-500">
+          Projects
+        </a>
+        <a className="text-2xl transition ease-in-out duration-200 hover:text-blue-500">
+          GARD
+        </a>
+        <a className="text-2xl transition ease-in-out duration-200 hover:text-blue-500">
+          MyTotem
+        </a>
+        <a className="text-2xl transition ease-in-out duration-200 hover:text-blue-500">
+          OneTrust
+        </a>
+        <a className="text-2xl transition ease-in-out duration-200 hover:text-blue-500">
+          Seize The Market
+        </a>
+        <a className="text-2xl transition ease-in-out duration-200 hover:text-blue-500">
+          HylasFit
+        </a>
+        <a className="text-3xl font-bold transition ease-in-out duration-200 hover:text-blue-500">
+          Resume
+        </a>
+        <a className="text-3xl font-bold transition ease-in-out duration-200 hover:text-blue-500">
+          Blog
+        </a>
+        <a className="text-3xl font-bold transition ease-in-out duration-200 hover:text-blue-500">
+          Instagram
+        </a>
       </div>
     </div>
   )
