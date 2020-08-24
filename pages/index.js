17c64f6a357147bf66f80d77cefc742e196c05f4
Chris Anderson <chris@fastmail.in>
2020-08-23 20:30:44 -0400

    Fetch all sorted posts with SSR

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const allPostsData = await getSortedPostsData()
   return {
     props: {
